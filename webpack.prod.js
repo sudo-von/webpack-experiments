4344e0a095747bea03c7ce1ebc4c2eab4c643188
V̵O̵N̵ <developerjesus@hotmail.com>
2020-12-22 22:50:07 -0600

    feat: Add webpack.dev.js and webpack.prod.js files, update package.json. Now package.json will be able to build a bundle based on the webpack config mode.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'production',
     entry: './index.js',
-    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
