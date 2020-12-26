5631bc5d5a43873bfdf2693d43e274b7a1557508
V̵O̵N̵ <developerjesus@hotmail.com>
2020-12-25 19:30:54 -0600

    feat: Update react code.

--- react/src/js/component/app.js
+++ react/src/js/component/app.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Loader from './loader';
+import data from './data.json';
 
 function App(){
+    const [loaderList, setLoaderList] = useState([])
+
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
+
     return (
         <div>
-            React JS!
+            ReactJS
+            <ul>
+                {
+                    loaderList.map(item => <Loader {...item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}>Show what i have learned</button>
         </div>
     );
 }
