4918ed10c3c2541c88d3c0bac1b2b26216ee56a7
V̵O̵N̵ <developerjesus@hotmail.com>
2020-12-25 18:19:17 -0600

    feat: Add modern javascript support

---
+++ babel/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCSsExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname,'src/js/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle-[name].js'
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        hot: true,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader',
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack-dev-deserver'
+        })
+    ]   
+}
